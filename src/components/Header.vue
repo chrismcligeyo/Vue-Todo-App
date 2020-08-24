7bb6564536e13b72f1ba63316a3d144247615a14
= <mcligeyochristopher@gmail.com>
2020-08-24 14:42:36 +0300

    Header Section complete

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+      <div id="header">
+        <h1>TODOLIST</h1>
+      </div>
+</template>
+
+<script>
+    export default {
+        name: 'Header',
+
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+#header{
+  /*background: #03ac13;*/
+  background: #41B883;
+  color: white;
+
+  h1{
+    text-align: center;
+  }
+}
+
+
+</style>
+
+

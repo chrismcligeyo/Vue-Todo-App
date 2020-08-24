2a69d876bef0c61f8d1117c5908116142aa963dd
= <mcligeyochristopher@gmail.com>
2020-08-24 17:56:58 +0300

    TodosListinitiated, added singletodo as component. each todo item can now be viewed

---
+++ src/components/TodosList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="">
+        <div v-for="todo in todos" :key="todo.id">
+            <SingleTodo :todo="todo"></SingleTodo>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    import SingleTodo from "./SingleTodo";
+
+    export default {
+        name: 'TodosList',
+
+        props: ["todos"],
+        components: {
+            SingleTodo,
+        }
+
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
+
+
